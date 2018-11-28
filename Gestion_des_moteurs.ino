d6100288d3bb613db89449ce4a3459500ed62421
MigAP <miguel.arpa.p@gmail.com>
2018-11-28 19:52:39 +0100

    initial commit

---
+++ Gestion_des_moteurs.ino
@@ -0,0 +1,39 @@
+
+//=============================================================================
+//                        DEFINITION DES FONCTIONS CONTROLE MOTEUR
+//=============================================================================
+
+
+//moteur1 : moteur gauche ?
+//moteur2: moteur droit ?
+
+void moteur1(unsigned int pwr, boolean sens) {
+  if (sens) {
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+  }
+  else {
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
+  }
+
+  analogWrite(ena, pwr);
+}
+
+void moteur2(unsigned int pwr, boolean sens) {
+  if (!sens) {
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4, LOW);
+  }
+  else {
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, HIGH);
+  }
+
+  analogWrite(enb, pwr);
+}
+
+void moteur_off() {
+  analogWrite(ena, 0);
+  analogWrite(enb, 0);
+}
